b0c6833bb5093e9eaaa2885d58f9e8c559248732
Alexis YOUF <alexis.youf@edu.ece.fr>
2021-10-28 19:01:31 +0200

    Get one user

--- cours6/myFirstApp/routes/users.js
+++ cours6/myFirstApp/routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
+
+
+//create RAW data array
+let users = [{
+  user: "Arthur",
+  id: "0"
+},
+];
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res) => {
+  //Get list of user and return JSON
+  res.status(200).json({users});
 });
 
+/*GET one user.*/
+router.get('/:id', (req, res) => {
+	// Get id in params
+	const { id } = req.params;
+	// Find movie in DB
+	const user = _.find(users, ["id", id]);
+	// Return user
+	res.status(200).json({
+		message: "User found !",
+		user
+	});
+}); 
 module.exports = router;
